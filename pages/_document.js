277ed8ffb9804f70fdd80cb0555e1e9d9f4eb6eb
keishi.i <gajyumaru1103@gmail.com>
2023-05-01 16:16:17 +0900

    darkmode-tutorial

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,55 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head></Head>
+        <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: themeInitializerScript,
+            }}
+          ></script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+const themeInitializerScript = `(function() {
+  ${setInitialColorMode.toString()}
+  setInitialColorMode();
+})()`;
+
+function setInitialColorMode() {
+  function getInitialColorMode() {
+    //ストレージからthemeを取得する
+    const storedPreferenceMode = window.localStorage.getItem("theme");
+    const hasStoredPreference = typeof storedPreferenceMode === "string";
+
+    if(hasStoredPreference) {
+      return storedPreferenceMode;
+    }
+
+    //現在のpreferance状態を確認します
+    const preference = window.matchMedia("(prefers-color-scheme: dark)");
+    const hasMediaQueryPreference = typeof preference.matches === "boolean";
+
+    if(hasMediaQueryPreference) {
+      return preference.matches ? "dark" : "light";
+    }
+    return "light";
+  }
+
+  const currentColorMode = getInitialColorMode();
+  console.log(currentColorMode);
+
+  if(currentColorMode === "dark") {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+}
+
+export default MyDocument;
